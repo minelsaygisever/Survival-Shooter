fe1ffbef263707226f18ada876437a1a7571fb55
Minel SAYGISEVER <minelsaygisever@gmail.com>
2018-08-07 20:09:25 +0300

    Chapter Three - Camera  Setup
    Made camera settings.

---
+++ Survival Shooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿/*
+Created By SAYGISEVER
+*/
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	#region Variables
+
+	public Transform target; // The position that that camera will be following.
+
+	public float smoothing = 5f; // The speed with which the camera will be following.
+
+	Vector3 offset;  // The initial offset from the target.
+
+
+	#endregion
+
+	private void Start()
+	{
+		// Calculate the initial offset.
+		offset = transform.position - target.position;
+	}
+
+	private void FixedUpdate()
+	{
+		// Create a postion the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp( transform.position, targetCamPos, smoothing * Time.deltaTime );
+	}
+
+	#region Methods
+	#endregion
+
+}
